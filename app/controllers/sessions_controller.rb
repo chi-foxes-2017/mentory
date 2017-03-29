eb2c0ea2e86c91105e5b6767653aa58dceaaacbe
Karl Thomas <email": null>
2017-03-29 16:59:42 -0500

    get styling from dbc style guid for our site

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to categories_path
+      redirect_to root_path
     else
       flash[:danger] = 'Invalid email/password combination'
       render 'new'
