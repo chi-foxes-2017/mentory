3de9b658ebfac1cce317f540c4c2b8f2094ec2e9
Karl Thomas <email": null>
2017-03-29 15:34:24 -0500

    create the users model with new and create routes, remember that it will have a show route later also have started work on the sessions controller for logging users in. might have to add a migration to include something to add mentoring functionality

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  include BCrypt
+
+  has_secure_password
+
+  validates :name, :email, :password_digest, presence: :true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   root 'splage#index'
 end
