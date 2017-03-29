5fea7f4f375af32edbd52796f6e830307f1f187a
Karl Thomas <email": null>
2017-03-29 16:25:48 -0500

    get the show page html done, working on routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    p params
+    return redirect_to new_session_path if !logged_in?
+    return redirect root_path if !authorized?(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
