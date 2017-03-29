1a35ec3f75eafb41512e5db3de28de90596c94d1
Elizabeth Hart & Kristen Moore <pair+elizabethhart.kmoreo@devbootcamp.com>
2017-03-29 18:28:30 -0500

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   root 'splage#index'
 
-  # resources :users do
-  #   resources :pairings
-  # end
-
-  get '/pairings' => 'pairings#index'
+  resources :users do
+    resources :pairings
+  end
 
 end
