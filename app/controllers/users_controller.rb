8fef41f581d74d1bd943f29d15c368798a0356ec
Elizabeth Hart & Kristen Moore <pair+elizabethhart.kmoreo@devbootcamp.com>
2017-03-30 10:17:50 -0500

    refactor seshcontroller with helper methods ** the log in method takes in an arg an set the sesh user id to it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      log_in(@user)
       redirect_to root_path
     else
       @errors = @user.errors.full_messages
