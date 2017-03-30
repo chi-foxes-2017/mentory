877eee05eebe48c9b9dd0035491c3a8f7a3c8b69
Elizabeth Hart & Kristen Moore <pair+elizabethhart.kmoreo@devbootcamp.com>
2017-03-30 10:50:18 -0500

    Add pairing confirmation email method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: 'notifications@mentory.com'
+
+  def pairing_confirmation_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Someone signed up to pair with you!')
+  end
 end
