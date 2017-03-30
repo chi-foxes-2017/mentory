f5e54086058fa91ed9603aaf55ed793a3c9d574c
Elizabeth Hart & Kristen Moore <pair+elizabethhart.kmoreo@devbootcamp.com>
2017-03-30 09:36:04 -0500

    add proper routes to rspec **before the integration tests were not written correctly because they did not account for the pairings being nested within dah users **running suite again

--- spec/routing/pairings_routing_spec.rb
+++ spec/routing/pairings_routing_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe "Routing to pairings", :type => :routing do
   let(:user) {User.create(name: "Karl", email: "karl@karl", password: "karl")}
 
   it "routes GET /pairings to pairings#index" do
-    expect(:get => root_path).to route_to("pairings#index")
+    expect(:get => "/users/#{(user.id)}/pairings").to route_to("pairings#index")
   end
 
   it "routes GET /pairings/1 to pairings#show" do
-    expect(:get => "/user/pairings/1").to route_to("pairings#show", :id => "1")
+    expect(:get => "/users/#{(user.id)}/pairings/1").to route_to("pairings#show", :id => "1")
   end
 
   it "routes GET /pairings/new to pairings#new" do
-    expect(:get => "/pairings/new").to route_to("pairings#new")
+    expect(:get => "/users/#{(user.id)}/pairings/new").to route_to("pairings#new")
   end
 
   it "routes GET /pairings/edit to pairings#edit" do
-    expect(:get => "/pairings/edit").to route_to("pairings#edit")
+    expect(:get => "/users/#{(user.id)}/pairings/1/edit").to route_to("pairings#edit", id: 1)
   end
 
   it "routes POST /pairings to pairings#create" do
-    expect(:post => "/pairings").to route_to("pairings#create")
+    expect(:post => "/users/#{(user.id)}/pairings").to route_to("pairings#create")
   end
 
   it "routes PUT /pairings to pairings#update" do
-    expect(:put => "/pairings").to route_to("pairings#update")
+    expect(:put => "/users/#{(user.id)}/pairings").to route_to("pairings#update")
   end
 
   it "routes DELETE /pairings/1 to pairings#destroy" do
-    expect(:delete => "/pairings/1").to route_to("pairings#destroy", :id => "1")
+    expect(:delete => "/users/#{(user.id)}/pairings/1").to route_to("pairings#destroy", :id => "1")
   end
 
 end
