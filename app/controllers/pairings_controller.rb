aa0c65d0beb83543ad012e56fe6b95175747f753
Elizabeth Hart & Kristen Moore <pair+elizabethhart.kmoreo@devbootcamp.com>
2017-03-30 17:00:20 -0500

    Add default icon url if user doesn't enter one

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -14,7 +14,7 @@ class PairingsController < ApplicationController
     @pairing = Pairing.new
   end
 
-  def update #post
+  def update
     @pairing = Pairing.find_by(id: params[:id])
     @mentor = User.find_by(id: @pairing.mentor_id)
     if params[:pairing][:topic]
@@ -40,7 +40,7 @@ class PairingsController < ApplicationController
     end
   end
 
-  def edit #get
+  def edit
     @user = User.find(params[:user_id])
     @pairing = Pairing.find(params[:id])
     if logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :name, :email, presence: :true
 
+  before_save: :default_values
+
   def current_offerings
     offerings.select { |off| off.start_time > Time.now }
   end
@@ -13,4 +15,11 @@ class User < ApplicationRecord
   def past_offerings
     offerings.select { |off| off.start_time < Time.now }
   end
+
+  def default_values
+    if self.url == nil
+      self.url = "https://ewedit.files.wordpress.com/2016/12/baby-groot.jpg?w=612"
+    end
+  end
+
 end
